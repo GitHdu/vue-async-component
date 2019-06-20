c38ee5521dcc84a7440c49c0f41e1cb1784033e1
Chao Yang <chaoyang@ChaodeMacBook-Pro.local>
2019-06-20 22:18:21 +0800

    feat: node生成文件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
+    <li v-for="i in arr">{{i.name}}</li>
     <button @click="toggle">toggle</button>
-    <async-component :path="path"
-                     a='test'></async-component>
+    <!-- <async-component :path="path"
+                     a='test'></async-component> -->
+    <!-- <ab v-if="test"></ab> -->
   </div>
 </template>
 
 <script>
 import asyncComponent from './components/async-component'
-
+function loadView (view) {
+  return () => import(/* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`)
+}
 export default {
   name: 'app',
   data () {
     return {
-      path: 'a.vue'
+      path: 'a.vue',
+      arr: [{ id: 'a', name: '1' }, { id: 'b', name: 2 }],
+      test: false
     }
   },
   methods: {
     toggle () {
-      this.path = 'b.vue'
+      this.arr.splice(1, 1, { id: 'c', name: 3 })
+      // this.path = 'b.vue'
+      // this.test = true
+    }
+  },
+  computed: {
+    a () {
+      this.arr[0].checked = true
     }
   },
   components: {
-    asyncComponent
+    asyncComponent,
+    ab: loadView('b')
+  },
+  created () {
+    console.log(this._self)
   }
 }
 </script>
--- src/views/a.vue
+++ src/views/a.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
     this is a
+    {{arr}}
   </div>
 </template>
 
 <script>
 export default {
   props: ['a'],
+  data () {
+    return {
+      arr: [1]
+    }
+  },
+  computed: {
+    t () {
+      return this.arr
+    }
+  },
   created () {
+    setTimeout(() => {
+      this.arr = Object.freeze([1, { a: 3 }])
+      this.$nextTick(() => {
+        console.log(this.t)
+      })
+    }, 1000)
     console.log(this.a)
   }
 }
